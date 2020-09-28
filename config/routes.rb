8208db15779a357c916eac7e33571cea2511cfbf
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-09-28 06:34:40 +0000

    [Add] Aboutのviesと、Aboutへのリンクの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   get root to: "homes#top"
+  get "home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
