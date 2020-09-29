7f7973de0507f9a980d21e0072feb5669c3193d9
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-09-29 07:42:37 +0000

    [Add] 部分テンプレートの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+  # 　devise_parameter_sanitizer.permit(:sign_in, keys: [:name,:passward])
   end
 
 end
