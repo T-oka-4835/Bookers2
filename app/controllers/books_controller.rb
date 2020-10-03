8d0894b6c68a1b366f0cbfb13e5486899be4337f
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-03 07:06:23 +0000

    [Finish] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,18 +22,26 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to @book, notice: 'You have created book successfully.'
     else
+      @books = Book.all
+      @user = current_user
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user.id != current_user.id
+      redirect_to books_path
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id), notice: 'You have updated book successfully.'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 before_action :authenticate_user!
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
     @book = Book.new
     @books = @user.books
   end
@@ -17,12 +17,18 @@ before_action :authenticate_user!
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user), notice: 'You have updated user successfully.'
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'You have updated user successfully.'
+    else
+      render :edit
+    end
   end
 
 private
