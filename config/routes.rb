19a249260e35293adf018b8e8132190102ac434f
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-18 03:22:37 +0000

    [Add Favoriteコントローラーとモデルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   get root to: "homes#top"
   get "home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
