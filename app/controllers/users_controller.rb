ce0cc5d6678788a4338f9bf32fe7536aea6ebd85
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-20 05:10:18 +0000

    [Add] UserとBookのプルダウンを追加（検索条件は修正が必要)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,9 +43,58 @@ before_action :authenticate_user!
     redirect_to root_path
   end
 
+  # 検索機能
+  def search
+    @model = params["search"]["model"]
+    @content = params["seach"]["content"]
+    @how = params["search"]["how"]
+    @dates = search_for(@how, @model, @content)
+  end
+  # 検索機能
+
+
 private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
+   # 検索機能
+   def match(model, content)
+     if model == 'user'
+       User.where(name:content)
+     elsif model == 'book'
+       Book.where(title:content)
+     end
+   end
+
+   def backward(model,content)
+     if model == 'user'
+       User.where("name LIKE?", "%#{content}")
+     elsif model == 'book'
+       Book.where("title LIKE", "%#{content}")
+     end
+   end
+
+   def partical(model, content)
+     if model == "user"
+       User.where("name LIKE?", "%#{content}%")
+     elsif model == "book"
+       Book.where("title LIKE?", "%#{content}%")
+     end
+   end
+
+   def search_for(how,model,content)
+     case how
+      when 'match'
+        match(model,content)
+      when 'forward'
+        forward(model,content)
+      when 'backward'
+         backward(model,content)
+      when 'partical'
+         partical(model,content)
+     end
+   end
+
+   # 検索機能
 end
