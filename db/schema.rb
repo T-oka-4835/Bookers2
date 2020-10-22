cfd8c3ae02ef7bbfe2386148ecfab9e3c38cddcb
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-10-22 07:37:39 +0000

    [Start]2ヶ月目の課題7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_145712) do
+ActiveRecord::Schema.define(version: 2020_10_22_073647) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_145712) do
     t.datetime "updated_at", null: false
     t.string "introduction"
     t.string "profile_image_id"
+    t.string "prefecture_code"
+    t.string "postal_code"
+    t.string "city"
+    t.string "building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
